0d387f578482b66718271f1902339223496bcf97
Ravi van Rooijen <github@ravithrasworld.nl>
2016-02-27 10:10:02 +0100

    First Start

---
+++ src/main/java/info/zthings/mcmods/madskullz/common/MadSkullz.java
@@ -0,0 +1,14 @@
+package info.zthings.mcmods.madskullz.common;
+
+import net.minecraft.init.Blocks;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+
+@Mod(modid = Ref.MODID, version = Ref.VERSION)
+public class MadSkullz
+{ 
+    @EventHandler
+    public void init(FMLInitializationEvent event) {
+    }
+}
---
+++ src/main/java/info/zthings/mcmods/madskullz/common/Ref.java
@@ -0,0 +1,6 @@
+package info.zthings.mcmods.madskullz.common;
+
+public abstract class Ref {
+	public static final String MODID = "madskullz";
+	public static final String VERSION = "1.0";
+}
