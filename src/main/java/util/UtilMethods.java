e04af08fcf7f5271c7c0e93457ee39757c5ec27f
Ravi van Rooijen <github@ravithrasworld.nl>
2016-02-28 19:05:58 +0100

    Completely forgot creeper head :O

--- src/main/java/util/UtilMethods.java
+++ src/main/java/util/UtilMethods.java
@@ -6,6 +6,7 @@ import java.io.IOException;
 
 import info.zthings.mcmods.madskullz.handlers.ConfigHandler;
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.monster.EntityCreeper;
 import net.minecraft.entity.monster.EntitySkeleton;
 import net.minecraft.entity.monster.EntityZombie;
 import net.minecraft.entity.player.EntityPlayer;
@@ -30,12 +31,14 @@ public abstract class UtilMethods {
 		ItemStack stack;
 		
 		//Setup & return skull-itemstack
-		if (entity instanceof EntitySkeleton || entity instanceof EntityZombie || entity instanceof EntityPlayer) { //ingame-skulls (or player skulls that don't have to be looked up in register)
+		if (entity instanceof EntitySkeleton || entity instanceof EntityZombie || entity instanceof EntityPlayer || entity instanceof EntityCreeper) { //ingame-skulls (or player skulls that don't have to be looked up in register)
 			if (entity instanceof EntitySkeleton) { //normal or wither skeleton
 				if (((EntitySkeleton)entity).getSkeletonType() == 1) stack = new ItemStack(Items.skull, 1, 1); //if wither skeleton drop that skull
 				else stack = new ItemStack(Items.skull, 1, 0); //for all other values drop standard-skeleton-skull
 			} else if (entity instanceof EntityZombie) {
 				stack = new ItemStack(Items.skull, 1, 2);
+			} else if (entity instanceof EntityCreeper) {
+				stack = new ItemStack(Items.skull, 1, 4);
 			} else { //must be player
 				NBTTagCompound nbt = new NBTTagCompound();
 				stack = new ItemStack(Items.skull, 1, 3);
