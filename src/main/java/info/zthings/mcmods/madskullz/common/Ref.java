a9c80df16fa6533ae1eb8d451e85df6769fbf108
Ravi van Rooijen <github@ravithrasworld.nl>
2016-03-02 17:54:53 +0100

    Update version

--- src/main/java/info/zthings/mcmods/madskullz/common/Ref.java
+++ src/main/java/info/zthings/mcmods/madskullz/common/Ref.java
@@ -4,7 +4,7 @@ import java.util.HashMap;
 
 public abstract class Ref {
 	public static final String MODID = "madskullz";
-	public static final String VERSION = "1.0";
+	public static final String VERSION = "BETA-1.1";
 	
 	public static final String DEBUG_SEPERATOR = "===============================================================";
 }
